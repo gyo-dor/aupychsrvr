6f3c27bd22f3c9edc671263b019224e52135944a
Gyodor Eostoevsky <probirds123@gmail.com>
2020-09-26 23:02:30 +0700

    Working API server

--- engine.py
+++ engine.py
@@ -1,7 +1,7 @@
+import traceback
 import chess
 import chess.engine
 import random
-import os
 
 
 def safe_response(func):
@@ -9,15 +9,16 @@ def safe_response(func):
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            return Response.error(str(e))
+            tb = f"{e} | {traceback.format_exc()}"
+            return Response.error(tb)
+
     return inner_function
 
 
 class ServerEngine:
-
     def __init__(self, engine_path: str, engine_config: dict):
-        """ Board and Engine instances are running on your local computer.
-            Engine path is relative path into your executable engine.
+        """Board and Engine instances are running on your local computer.
+        Engine path is relative path into your executable engine.
         """
         self.engine_path = engine_path
         self.engine_config = engine_config
@@ -45,17 +46,23 @@ class ServerEngine:
         else:
             new_fen = new_fen.replace(" w ", " b ")
         self.board = chess.Board(new_fen)
-        return Response.success(f"Overwrite FEN success. Current active FEN: [{str(self.board.fen())}]")
+        return Response.success(
+            f"Overwrite FEN success. Current active FEN: [{str(self.board.fen())}]"
+        )
 
     @safe_response
     def use_this_fen(self, fen):
         self.board = chess.Board(fen)
-        return Response.success(f"Use this FEN success. Current active FEN: [{str(self.board.fen())}]")
+        return Response.success(
+            f"Use this FEN success. Current active FEN: [{str(self.board.fen())}]"
+        )
 
     @safe_response
     def move(self, notation):
-        assert len(notation) == 4 or len(notation) == 5, f"Move notation must 4 or 5, got {notation}"
-        
+        assert (
+            len(notation) == 4 or len(notation) == 5
+        ), f"Move notation must 4 or 5, got {notation}"
+
         if self.board.is_game_over():
             return Response.error("Cannot move because game is over.")
 
@@ -63,15 +70,15 @@ class ServerEngine:
         return Response.success()
 
     @safe_response
-    def get_best_move(self, thinking_time=0.3):        
+    def get_best_move(self, thinking_time=0.3):
         limits = chess.engine.Limit(time=thinking_time)
         return self.on_get_engine_movement(limits)
 
     @safe_response
     def get_best_move_by_clock(self, white_clock, black_clock, increment=0):
-        if increment != 0: 
+        if increment != 0:
             raise NotImplementedError
-        
+
         limits = chess.engine.Limit(white_clock=white_clock, black_clock=black_clock)
         return self.on_get_engine_movement(limits)
 
@@ -79,29 +86,29 @@ class ServerEngine:
     def on_get_engine_movement(self, limits):
         if self.board.is_game_over():
             return Response.error("Cannot get best movement because game is over.")
-        
+
         try:
             engine_result = self.engine_instance.play(self.board, limits, ponder=True, info=chess.engine.INFO_SCORE)
         except Exception as e:
             self.initialize_engine()
             return Response.error(f"On Get Engine Movement: {e}")
-        
+
         return self.on_get_movement_response(engine_result)
 
     @safe_response
-    def on_get_movement_response(self, result):
+    def on_get_movement_response(self, engine_result):
         message = ""
-        
-        if result.resigned:
+
+        if engine_result.resigned:
             message = "WARNING - Engine resigned. Trying to reinitialize engine again."
             self.initialize_engine()
-        
+
         return Response.move(
-            movement=result.move_uci(),
-            score=str(result.info["score"].relative),
-            is_resigned=result.resigned,
-            is_draw_offered=result.draw_offered,
-            message=message
+            movement=engine_result.move.uci(),
+            score=str(engine_result.info["score"].relative),
+            is_resigned=engine_result.resigned,
+            is_draw_offered=engine_result.draw_offered,
+            message=message,
         )
 
     @safe_response
@@ -109,12 +116,13 @@ class ServerEngine:
         self.engine_instance.close()
         self.engine_instance = None
         return Response.success("Engine on finish success.")
-    
+
 
 class Response:
-    
     @staticmethod
-    def move(movement: str, score: str, is_resigned: bool, is_draw_offered: bool, message=""):
+    def move(
+        movement: str, score: str, is_resigned: bool, is_draw_offered: bool, message=""
+    ):
         return {
             "success": 1,
             "message": message,
@@ -122,8 +130,8 @@ class Response:
                 "move": movement,
                 "score": score,
                 "is_draw_offered": is_draw_offered,
-                "is_resigned": is_resigned
-            }
+                "is_resigned": is_resigned,
+            },
         }
 
     @staticmethod
@@ -131,7 +139,7 @@ class Response:
         return {
             "success": 1,
             "fen": fen,
-            "message": f"Get current FEN success. FEN: [{fen}]"
+            "message": f"Get current FEN success. FEN: [{fen}]",
         }
 
     @staticmethod
@@ -139,8 +147,8 @@ class Response:
         return {
             "success": 0,
             "message": reason
-        }
-        
+            }
+
     @staticmethod
     def success(message=""):
         return {
--- main.py
+++ main.py
@@ -10,23 +10,33 @@ if "nt" in os.name:
 
 engine: ServerEngine = None
 
+def initialize_engine_first(func):
+    def inner_function(*args, **kwargs):
+        return func(*args, **kwargs) if engine is not None else Response.error("Engine is not initialized.")
+    return inner_function
+
 # TODO Passed testing can play more than 2 games with same engine lifecycle.
+
+
+@app.get("/ping")
+def ping():
+    return Response.success("Pong!")
 
 
 @app.post("/on_initialize_engine")
 def on_initialize_engine(config: dict):
     global engine
     message = ""
-    
+
     try:
         if isinstance(engine, ServerEngine):
-            message += "Detected running engine instance, but successfully terminated."
+            message += "Detected running engine instance, but successfully terminated. "
             engine.on_finish()
 
         engine = ServerEngine(engine_path, config)
         message += "Engine initialized."
         return Response.success(message)
-    
+
     except Exception as e:
         return Response.error(str(e))
 
@@ -38,29 +48,37 @@ def on_move(notation: str):
 
 @app.post("/get_best_move")
 def on_get_best_move(thinking_time: float):
-    return engine.get_best_move(thinking_time=thinking_time)
+    return (
+        engine.get_best_move(thinking_time=thinking_time) if engine is not None
+        else Response.error("Engine is not initialized.")
+    )
 
 
 @app.post("/get_best_move_by_clock")
 def on_get_best_move_by_clock(white_clock: float, black_clock: float):
-    return engine.get_best_move_by_clock(white_clock, black_clock)
-
+    return (
+        engine.get_best_move_by_clock(white_clock, black_clock) if engine is not None 
+        else Response.error("Engine is not initialized.")
+    )
+    
 
 @app.get("/on_finish")
 def on_finish():
-    return engine.on_finish()
-
-
-@app.get("/overwrite_fen/{side}/{fen}")
-def overwrite_fen(side: str, fen: str):
-    return engine.overwrite_fen(fen, side)
-
-
-@app.get("/use_this_fen/{fen}")
-def use_this_fen(fen: str):
-    return engine.use_this_fen(fen)
+    return engine.on_finish() if engine is not None else Response.error("Engine is not initialized.")
 
 
 @app.get("/get_current_fen")
 def get_current_fen():
-    return engine.get_current_fen()
+    return engine.get_current_fen() if engine is not None else Response.error("Engine is not initialized.")
+
+
+@app.post("/overwrite_fen")
+def overwrite_fen(side: str, fen: str):
+    return engine.overwrite_fen(fen, side) if engine is not None else Response.error("Engine is not initialized.")
+
+
+@app.post("/use_this_fen")
+def use_this_fen(fen: str):
+    return engine.use_this_fen(fen) if engine is not None else Response.error("Engine is not initialized.")
+
+
