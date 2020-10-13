ef13c068036979d09866ba61e102d1aca0ac5107
Gyodor Eostoevsky <probirds123@gmail.com>
2020-10-13 11:28:50 +0700

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -32,12 +32,10 @@ echo "###############################################"
 echo "Downloading and configuring Stockfish Chess Engine"
 echo "###############################################"
 
+wget https://abrok.eu/stockfish/builds/767b4f4fbe5ab2e63aceabd9005f4e1eb7cbcb51/linux64bmi2/stockfish_20100519_x64_bmi2.zip
 filename='stockfish_20100519_x64_bmi2'
 
-wget https://abrok.eu/stockfish/builds/767b4f4fbe5ab2e63aceabd9005f4e1eb7cbcb51/linux64bmi2/stockfish_20100519_x64_bmi2.zip
 unzip "$filename.zip"
-rm engines/linux
-rm engines/windows
-mkdir engines/linux
 mv $filename engine
 mv engine engines/linux
+rm "$filename.zip"
